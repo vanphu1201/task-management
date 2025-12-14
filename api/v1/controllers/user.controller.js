d34afbf2dcd6ed8c77ce1ab09df6513a548e5705
vanphuTteam <vanphu120107@gmail.com>
2025-12-14 09:09:55 +0700

    tính năng đăng nhập

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -37,3 +37,46 @@ module.exports.register = async (req, res) => {
         })
     }
 }
+
+
+// [POST] /api/v1/users/login
+module.exports.login = async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = md5(req.body.password);
+        const user = await User.findOne({
+            email: email,
+            deleted: false
+        })
+
+        if (!user) {
+            res.json({
+                code: 400,
+                message: "Email không tồn tại!"
+            })         
+            return;
+        }
+
+        if (password !== user.password) {
+            res.json({
+                code: 400,
+                message: "Mật khẩu không đúng!"
+            })         
+            return;
+        }
+        
+        const token = user.token;
+        res.cookie("token", token);
+
+        res.json({
+            code: 200,
+            message: "Đăng nhập thành công!",
+            token: token
+        })
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Lỗi đăng nhập thất bại!"
+        })
+    }
+}
--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -5,7 +5,7 @@ const controller = require("../controllers/user.controller");
 
 route.post("/register", controller.register);
 
-
+route.post("/login", controller.login);
 
 
 module.exports = route;
